b2f37a3c2a7a61159f09c338feb0cf59b509d3df
William Mattson Kheel Gomes Araújo <157336038+Willhoooo@users.noreply.github.com>
2025-08-05 19:01:43 -0400

    Create dicionario.py
    adicionei mais um arquivo.py da aula sobre dicionários que tivemos.

---
+++ dicionario.py
@@ -0,0 +1,15 @@
+dic1 = {
+    'nome': 'João',
+    'idade': 25,
+    'cidade': 'Gramado'
+}
+
+dic2 = {
+    'nome': 'Pedro',
+    'idade': 17,
+    'cidade': 'Manaus',
+    'hobbies': 'Futebol'
+}
+
+print(dic1.values())
+print(dic2.values())
